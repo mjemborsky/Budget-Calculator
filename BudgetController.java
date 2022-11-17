f0f6099e5f461120965b9abfca0da75bac9c5ac4
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-11-16 22:24:47 -0500

    Add files via upload
    This is to be used as a template for other stages. Holds the menu and "logo". Still working on some effects like hovering over buttons.

---
+++ BudgetController.java
@@ -0,0 +1,41 @@
+package com.example.budgetcalculator;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+
+public class BudgetController implements Initializable {
+
+    @FXML
+    private Button Business;
+
+    @FXML
+    private ImageView menuClose;
+
+    @FXML
+    private ImageView Menu;
+
+    @FXML
+    private Button Personal;
+
+    @FXML
+    private Button Settings;
+
+    @FXML
+    private VBox menuSide;
+
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+
+
+}
